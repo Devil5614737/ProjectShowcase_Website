3e4147dfc61187351d433d526c2451fec6b54404
kaushik <83204057+Devil5614737@users.noreply.github.com>
2021-12-16 21:59:18 +0530

    Update [code].jsx

--- pages/[code].jsx
+++ pages/[code].jsx
@@ -1,1 +1,16 @@
 
+const RedirectURL = () => {
+    return null;
+   }
+   
+   export default RedirectURL;
+
+
+
+
+
+   export const getServerSideProps = async (context) => {
+    const { res } =  context;
+    res.writeHead(301, { location: "https://google.com" } );
+    res.end();
+  }
