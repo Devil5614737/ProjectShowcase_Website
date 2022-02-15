2ba5fe96e836c1775607dc63acd15d52198ad5a4
kaushik <kaushiksheel9@gmail.com>
2022-02-15 21:55:57 +0530

    .

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,16 +4,18 @@
  
  const Navbar=()=>{
 return(
-<div className={styles.navbar}>
-    <p className={styles.logo}>
-        Code<span className={styles.span}>X</span>
-    </p>
-    <div className={styles.links}>
-        <ul className={styles.links_ul}>
-            <li className={styles.links_li}><a href="#home" className={styles.link}>Home</a></li>
-            <li className={styles.links_li}><a href="#projects" className={styles.link}>Projects</a></li>
-            <li className={styles.links_li}><a href="#contact" className={styles.link}>Contact</a></li>
-        </ul>
+<div class="navbar">
+    <div className={styles.navbarContainer}>
+        <p className={styles.logo}>
+            Code<span className={styles.span}>X</span>
+        </p>
+        <div className={styles.links}>
+            <ul className={styles.links_ul}>
+                <li className={styles.links_li}><a href="#home" className={styles.link}>Home</a></li>
+                <li className={styles.links_li}><a href="#projects" className={styles.link}>Projects</a></li>
+                <li className={styles.links_li}><a href="#contact" className={styles.link}>Contact</a></li>
+            </ul>
+        </div>
     </div>
 </div>
 
